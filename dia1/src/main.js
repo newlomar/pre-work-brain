d2942fba091bfa7e6ee8a1d07452d67f2b14dd67
Newton Lomar <newtonlomar16@gmail.com>
2021-08-17 21:04:58 -0300

    resolucao do exercicio 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,28 @@
 import './style.css'
 
-document.querySelector('[data-js=app]').innerHTML = `
+const app = document.querySelector('[data-js=app]');
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const anchor = document.querySelector('[data-js=anchor]');
+
+let flag = 1;
+
+anchor.addEventListener('click', (event) => {
+
+  console.log(flag);
+
+  event.preventDefault();
+  app.classList.toggle('hide');
+
+  if (!(flag % 2 === 0)) {
+    anchor.innerHTML = 'Fazer o conteúdo aparecer';
+  }
+  else {
+    anchor.innerHTML = 'Sumir com o conteúdo';
+  }
+
+  flag++;
+})
